1116e285e933bbd5b7fbc14a7ad7feab10b5a73d
ItsAsadullah <itsasadullah@github.com>
2026-06-10 00:46:05 +0600

    Clean up unused test files and scripts

--- clear-limits.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function clearLimits() {
-  await prisma.$executeRawUnsafe('TRUNCATE TABLE ip_rate_limits;');
-  console.log('IP Rate limits cleared!');
-}
-
-clearLimits().catch(console.error).finally(() => prisma.$disconnect());
